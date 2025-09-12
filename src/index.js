cb3be5668e0b7b5004b434b63374594006ce845b
ShaunDM <smcra49@gmail.com>
2025-09-12 08:20:52 -0400

    Update index.js
    Attempted fix for bad routing.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const root = createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/">
       <App />
     </Router>
   </React.StrictMode>
