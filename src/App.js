3e38e8c3cae8d15b80811137d4ee752cc9a48349
ShaunDM <smcra49@gmail.com>
2025-09-12 08:27:23 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,11 +17,11 @@ function App() {
       <Route element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="portfolio" element={<Portfolio />} />
-        <Route path="books" element={<Books />} />
-        <Route path="music" element={<Music />} />
-        <Route path="movies_tv" element={<MoviesTV />} />
-        <Route path="games" element={<Games />} />
-        <Route path="contact_me" element={<ContactMe />} />
+        <Route path="/books" element={<Books />} />
+        <Route path="#/music" element={<Music />} />
+        <Route path="/movies_tv" element={<MoviesTV />} />
+        <Route path="/games" element={<Games />} />
+        <Route path="/contact_me" element={<ContactMe />} />
         <Route element={<ErrorPage />} />
       </Route>
     </Routes>
