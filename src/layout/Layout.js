434a167521fb465faa71beeb4464be9d30fc98ea
ShaunDM <smcra49@gmail.com>
2025-09-16 08:44:34 -0400

    Still attempting to fix

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -6,74 +6,25 @@ import Footer from "./Footer";
 import Main from "./Main";
 import Header from "./Header";
 import ScrollToTop from "./ScrollToTop";
-import loadMultipleFiles from "../util/loadMultipleFiles";
 
 function Layout() {
-  const [path, setPath] = useState(window.location.hash.substring(1));
-  const [index, setIndex] = useState(0);
-  const [format, setFormat] = useState(true);
-  const [assets, setAssets] = useState(loadMultipleFiles(path));
-  const handleSelectIndex = (selectedIndex) => {
-    setIndex(selectedIndex);
-  };
-  const handleSelectFormat = (selectedFormat) => {
-    if (selectedFormat)
-      setAssets({ ...assets, sidebar: { ...assets.sidebar, type: "list" } });
-    else
-      setAssets({
-        ...assets,
-        sidebar: { ...assets.sidebar, type: "carousel" },
-      });
-    setFormat(selectedFormat);
-  };
-
-  const handleSelectPath = (selectedPath) => {
-    setPath(selectedPath);
-  };
-
-  useEffect(() => {
-    function getAssets() {
-      setAssets(loadMultipleFiles(path));
-    }
-
-    if (path.substring(1) !== assets.origin) {
-      getAssets();
-    }
-
-    return () => setAssets(null);
-  }, [assets]);
-
-  console.log(path, assets);
-
   const footerStyle = path === "/" ? "position-bottom" : null;
 
   return (
-    <PropContext.Provider
-      value={{
-        assets: assets,
-        index: index,
-        handleSelectIndex: handleSelectIndex,
-        format: format,
-        handleSelectFormat: handleSelectFormat,
-        path: path,
-        handleSelectPath: handleSelectPath,
-      }}
-    >
-      <Container className="contr" fluid>
-        <Row>
-          <Header />
-        </Row>
-        <Row>
-          <Main>
-            <Outlet />
-          </Main>
-        </Row>
-        <Row className={footerStyle}>
-          <Footer />
-        </Row>
-        <ScrollToTop />
-      </Container>
-    </PropContext.Provider>
+    <Container className="contr" fluid>
+      <Row>
+        <Header />
+      </Row>
+      <Row>
+        <Main>
+          <Outlet />
+        </Main>
+      </Row>
+      <Row className={footerStyle}>
+        <Footer />
+      </Row>
+      <ScrollToTop />
+    </Container>
   );
 }
 
