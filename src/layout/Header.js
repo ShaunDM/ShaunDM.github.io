3b3c6e35662700a1f46920575ab07fbb79a16e6d
ShaunDM <smcra49@gmail.com>
2025-10-09 17:46:22 -0400

    Banner fix, mobile
    On a cellular device, banner was moved positive instead negative. CSS changed fixed issue.

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,18 +1,19 @@
+import { useContext } from "react";
+import { PropContext } from "./PropContext";
 import { Row } from "react-bootstrap";
 import loadMultipleFiles from "../util/loadMultipleFiles";
 
 //renders header, static throughout all routes.
 function Header() {
   const headerAssets = loadMultipleFiles("header");
+
   return (
     <header name="header" id="header" className="contain">
       <Row className="header-banner contain">
         <div className="header-image-container contain">
           <div
             className="header-image contain"
-            style={{
-              backgroundImage: "url(" + headerAssets.banner + ")",
-            }}
+            style={{ backgroundImage: "url(" + headerAssets.banner + ")" }}
           >
             <h1
               aria-hidden="true"
